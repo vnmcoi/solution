a48ab0efaed342802bc6e47e73b6bb5f70ef9c98
Hien Tran <214645525+htran404@users.noreply.github.com>
2026-03-09 23:47:26 +0700

    Commit

---
+++ src/MATRIX3.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int mxN = 2025 + 5;
+
+int N, M;
+int A[mxN][mxN];
+int f[mxN][mxN][3];
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    freopen("MATRIX3.INP", "r", stdin);
+    freopen("MATRIX3.OUT", "w", stdout);
+
+    cin >> N >> M;
+    for (int i = 1; i <= N; ++i)
+    {
+        for (int j = 1; j <= M; ++j)
+        {
+            cin >> A[i][j];
+        }
+    }
+    f[1][1][0] = A[1][1];
+    for (int i = 1; i <= N; ++i)
+    {
+        for (int j = 1; j <= M; ++j)
+        {
+            if (i == 1 && j == 1)
+            {
+                continue;
+            }
+            if (j - 1 >= 1)
+            {
+                for (int k = 0; k <= 2; ++k)
+                {
+                    f[i][j][0] = max(f[i][j][0], f[i][j - 1][k] + A[i][j]);
+                }
+            }
+            if (i - 1 >= 1)
+            {
+                for (int k = 1; k <= 2; ++k)
+                {
+                    f[i][j][k] = max(f[i][j][k], f[i - 1][j][k - 1] + A[i][j]);
+                }
+            }
+        }
+    }
+    cout << max(f[N][M][0], max(f[N][M][1], f[N][M][2]));
+    return 0;
+}
